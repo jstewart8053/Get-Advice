9f278b11aad160750e9798063b0c6669e5c8c186
Jennifer Stewart <jstewart8053@gmail.com>
2023-11-24 21:28:50 -0500

    added useEffect and props

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
 export default function App() {
   const [advice, setAdvice] = useState("");
-  const [read, setRead] = useState(0);
+  const [count, setCount] = useState(0);
 
   async function getAdvice() {
     const res = await fetch("https://api.adviceslip.com/advice");
     const data = await res.json();
     setAdvice(data.slip.advice);
-    setRead(read + 1);
+    setCount((c) => c + 1);
   }
+
+  useEffect(function () {
+    getAdvice();
+  }, []);
 
   return (
     <div>
-      <h1>Free Advice</h1>
-      <button onClick={getAdvice}>Get Advice </button>
-      <p>{advice}</p>
-      <p>
-        {" "}
-        You have read <strong>{read}</strong> pieces of advice{" "}
-      </p>
+      <h1>{advice}</h1>
+      <button onClick={getAdvice}>Get advice</button>
+      <Message count={count} />
     </div>
+  );
+}
+
+function Message(props) {
+  return (
+    <p>
+      You have read <strong>{props.count}</strong> pieces of advice
+    </p>
   );
 }
